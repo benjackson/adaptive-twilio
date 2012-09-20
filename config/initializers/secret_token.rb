404ab3489967c8c4b7b72d0c18192e0abea73095
Ben Jackson <ben@lissomsolutions.com>
2012-09-20 10:20:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdaptiveTwilio::Application.config.secret_token = '957a6de8d4c9542ee89cc504ad502a9705e1cd98d3a738c8c88c8fd39814bb659b373c194e1ede8740be977da1e9da02d9d1fa9f434202eb9a0152223492f266'
