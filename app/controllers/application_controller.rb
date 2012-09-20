76122696fe1afb614155ead1c8bfefb79a6e66ab
Joe Root <joe@onlysix.co.uk>
2012-09-20 12:49:26 +0100

    Application controller method for client (twilio_client).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def twilio_client
+    @client = @client || Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
+    return @client
+  end
 end
