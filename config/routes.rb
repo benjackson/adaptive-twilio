ad7bfd78a35ecae0a75ca47b58677034f92b4ade
Joe Root <joe@onlysix.co.uk>
2012-09-20 11:32:32 +0100

    Removed calls bug.

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ AdaptiveTwilio::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
  
-  resources :photos, :only => [:show]
+  resources :calls, :only => [:show]
 
   # Sample resource route with options:
   #   resources :products do
